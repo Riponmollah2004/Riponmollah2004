28c1216091dbcc87398561b7ace61a9a5ef11d4e
Riponmollah2004 <89471926+Riponmollah2004@users.noreply.github.com>
2022-02-02 11:45:49 +0600

    Update Termux-Login.sh

--- Termux-Login.sh
+++ Termux-Login.sh
@@ -1,9 +1,20 @@
 #!/user/bin/bash
 #Author : Mohammad Ripon
+setup_system(){
+	trap ' ' 2 15 20
+	clear
+	read -p "`printf \"\033[0;96m ~ \033[0;97m\$ Setup Security Name: \"`"  name
+	read -p "`printf \"\033[0;96m ~ \033[0;97m\$ Setup Password: \"`"  password
+	echo "$name">$PREFIX/bin/owner.db
+	echo "$password">$PREFIX/bin/passw.db
+	clear
+	login_system
+	trap - 2 15 20
+}
 done_login(){
 clear
 printf "\033[0;92m"
-figlet "REALME"
+figlet "`cat $PREFIX/bin/owner.db`"
 printf "\033[0m"
 cd /sdcard/
 }
@@ -13,7 +24,7 @@ clear
 printf "\033[0;92m"
 figlet "REALME"
 read -p "`printf \"\033[0;96m ~ \033[0;97m\$ Enter Password: \"`" passw
-if [ "$passw" = "772233" ]
+if [ "$passw" = "`cat $PREFIX/bin/passw.db`" ]
 then
     touch "$PREFIX/tmp/login"
     done_login
@@ -22,6 +33,12 @@ else
 fi
 trap - 2 15 20
 }
+FILE="$PREFIX/bin/owner.db"
+if [ -f "$FILE" ]; then
+    clear
+else
+    setup_system
+fi
 
 FILE="$PREFIX/tmp/login"
 if [ -f "$FILE" ]; then
